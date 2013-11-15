c601542cb418c4a903d5a62d5d528bb1d188d280
Valerie Poser <s9vapose@stud.uni-saarland.de>
2013-11-15 12:47:06 +0100

    added exampleHistory

---
+++ php/exampleHistory.php
@@ -0,0 +1,28 @@
+<?php
+
+include("algorithm.php");
+
+$m00 = array ("add", 12);
+$m01 = array ("rev", 11);
+$m02 = array ("clo", 100);
+$m03 = array ("new", 120);
+$m04 = array ("add", 19);
+$m05 = array ("aus", 39);
+$m06 = array ("asd", 86);
+$m07 = array ("adf", 98);
+$m08 = array ("add", 124);
+$m09 = array ("iuf", 531);
+$m11 = array ("udj", 86);
+$m12 = array ("asd", 35);
+$m13 = array ("add", 86);
+$m14 = array ("add", 184);
+$m15 = array ("new", 35);
+$m16 = array ("ifv", 8);
+$m17 = array ("ius", 63);
+$m18 = array ("idf", 12);
+$m19 = array ("inj", 168);
+
+$input = array($m00, $m01, $m02, $m03, $m04, $m05, $m06, $m07, $m08, $m09, $m11, $m12, $m13, $m14, $m15, $m16, $m17, $m18, $m19);
+echo "Ich laufe bis hier hin";
+algorithm($input,0);
+?>
