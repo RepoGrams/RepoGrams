b2549875b70f190e11647de9d987b4a8be20e561
Maike Maas <s9memaas@stud.uni-saarland.de>
2013-11-15 13:33:43 +0100

    bug fix

--- php/algorithm.php
+++ php/algorithm.php
@@ -1,23 +1,16 @@
 <?php
-
-		$letter_Array = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-
-		$number_Array = array("y", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
-
 	
-	class Algorthim {
+	class algorithm {
 
-		function algorithm($commitArray, $modus = 0){ //array looks like this [[$msg, $diff],[$msg, $diff],[$msg, $diff]]
+		public function render($commitArray, $modus = 0){ //array looks like this [[$msg, $diff],[$msg, $diff],[$msg, $diff]]
 
 			$count = count($commitArray);
-
-			$huge = ((string) $count)[0];
 
 			$all_diff = 0;
 
 			for ($j = 0; $j < $count; $j++){
 
-				$all_diff += $commitArray[j][1]; 
+				$all_diff += $commitArray[$j][1]; 
 
 				################################
 				### negativen diff behandeln ###
@@ -37,10 +30,12 @@
 			$z = $hohe;	#rechts unten -> oben HÖHE
 
 			$pixel = $width * $height; #all pixels on picture
-			$factor = ($pixel/$hohe) / $diff2;
+			$factor = ($pixel/$hohe) / $all_diff;
 
 			for ($i = 0; $i < $count; $i++){
-				$color = commitToColor($modus, commitArray[$i][0], commitArray[$i][1], $img);
+				$str = $commitArray[$i][0];
+				$color = $this->commitToColor($modus, $str, $img);
+ 		 		#$color = ImageColorAllocate($img, 100, 100, 100);
  		 		$w = ($x+($diff*$factor));
 				ImageFilledRectangle($img, $x, $y, $w, $z, $color); 
 				if ($w > $width){
@@ -56,23 +51,26 @@
 					$x += $diff*$factor;
 				}
 			}
+
+			imagepng($img, "name.png");
+			return $img;
 		}
 
-		function commitToColor($modus, $msg, $img){
-			if (§msg == null or strlen($msg) == 0)
+		private function commitToColor($modus, $msg, $img){
+			if ($msg == null or strlen($msg) == 0)
 		      			return ImageColorAllocate($img, 211, 211, 211);
-		    $msg=preg_replace("/[^a-zA-Z0-9 ]/" , "" , $msg);
+		    $msg = preg_replace("/[^a-zA-Z0-9 ]/" , "" , $msg);
 		    $msg = strtolower($msg);
 		    switch ($modus) {
 				case 0:
 		    		$first = substr($msg, 0, 1);
-		    		$r = letterValue($first, 0)
+		    		$r = $this->letterValue($first, 0);
 		    		if (strlen($msg) > 1){
 		    			$second = substr($msg, 1, 1);
-		    			$g = 0.3 + 0.6 * letterValue ($second, 1);
+		    			$g = 0.3 + 0.6 * $this->letterValue ($second, 1);
 		    			if (strlen($msg) > 2) {
 		    				$third = substr($msg, 2, 1);
-		    				$b = 0.4 + 0.5 * letterValue ($third, 2);
+		    				$b = 0.4 + 0.5 * $this->letterValue ($third, 2);
 		    			}
 		    			else {
 		    				$b = 0;
@@ -84,10 +82,10 @@
 		    		$color = ImageColorAllocate($img, $r, $g, $b);
 		    		return $color;
 				case 1:
-					/* hier muss der String noch zerteilt werden */
-					for ($i=0; $i < $anzahl; $i++) { 
-						#array_search($stringRep[i], $keyword_Array)
-					}
+					// /* hier muss der String noch zerteilt werden */
+					// for ($i=0; $i < $anzahl; $i++) { 
+					// 	#array_search($stringRep[i], $keyword_Array)
+					// }
 					break;
 				default:
 					echo "Hier läuft was schief.";
@@ -96,18 +94,23 @@
 		}
 
 
-		function letterValue($letter) {
-			if is_numeric($letter){
-		    	$value = 22 * array_search($letter, $number_Array);
+		private function letterValue($letter) {
+
+			$letterArray = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+
+			$numberArray = array("y", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+
+
+			if (is_numeric($letter)){
+		    	$value = 22 * array_search($letter, $numberArray);
 			}
 			else{
-		    	$value = 8 * array_search($letter, $letter_Array);
+		    	$value = 8 * array_search($letter, $letterArray);
 		    }
 		    return $value;
 		}
 
 	}
 
-	$algo = new Algorithm();
 
 ?>
--- php/exampleHistory.php
+++ php/exampleHistory.php
@@ -1,7 +1,7 @@
 <?php
 
 include("algorithm.php");
-
+$algo = new algorithm();
 $m00 = array ("add", 12);
 $m01 = array ("rev", 11);
 $m02 = array ("clo", 100);
@@ -23,6 +23,7 @@ $m18 = array ("idf", 12);
 $m19 = array ("inj", 168);
 
 $input = array($m00, $m01, $m02, $m03, $m04, $m05, $m06, $m07, $m08, $m09, $m11, $m12, $m13, $m14, $m15, $m16, $m17, $m18, $m19);
-echo "Ich laufe bis hier hin";
-algorithm($input,0);
+echo "<htlm><head></head><body>";
+echo "<img src=\"".$algo->render($input,0)."\" />";
+echo "</body></html>";
 ?>
