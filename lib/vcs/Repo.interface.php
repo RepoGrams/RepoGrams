d79b7c864b47b358390021c9bba27442026a1efb
Fabian Kosmale <fakos@t-online.de>
2013-11-15 12:37:08 +0100

    specified exceptions in interface

--- lib/vcs/Repo.interface.php
+++ lib/vcs/Repo.interface.php
@@ -6,21 +6,29 @@ interface Repo_Interface
 {
 	/**
 	 * @return an array containing the name of all branches
+	 * @throws An exception e; e->getMessage() contains a description of 
+	 * what have failed
 	 */
 	public function ListBraches();
 
 	/**
 	 * Changes the current branch to $branch
+	 * @throws An exception e; e->getMessage() contains a description of 
+	 * what have failed
 	 */
 	public function SwitchToBranch($branch);
 
 	/**
 	 * @return a commit object, representing the first commit
+	 * @throws An exception e; e->getMessage() contains a description of 
+	 * what have failed
 	 */
 	public function GetFirstCommit();
 
 	/**
 	 * @return an array containing all git objects
+	 * @throws An exception e; e->getMessage() contains a description of 
+	 * what have failed
 	 */
 	public function GetAllCommits();
 
