65e047e6486d3f43baca5ba917d4db20461a0331
Fabian Kosmale <fakos@t-online.de>
2013-11-22 10:33:24 +0100

    call callback once

--- lib/vcs/RepoFactory.class.php
+++ lib/vcs/RepoFactory.class.php
@@ -1,8 +1,11 @@
 <?php
 require_once "git/GitRepo.class.php";
+require_once "../../php/action.php";
 
 class RepoFactory {
 	static function createRepo($url, $callback) {
+		$act = new action();
+		$act->$callback("test");
 		return new GitRepo($url);
 	}
 }
