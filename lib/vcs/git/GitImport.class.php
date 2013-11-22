47c3678447a5cc4643cb8a398036aed00c0a7c76
root <root@sebbecking.de>
2013-11-22 15:59:53 +0100

    updated sanitazion

--- lib/vcs/git/GitImport.class.php
+++ lib/vcs/git/GitImport.class.php
@@ -5,8 +5,9 @@ class gitImport {
 	private $RepoObject;
 
 	public function __construct($repo,$user=null,$password=null){
-		if ($this->validateURL($repo) === false){
-		die("<h1>Possible injection detected</h1>");
+		if ($this->is_valid_url($repo) === false){
+			// die("<h1>Possible injection detected</h1>");
+			error_log("Injection detected: $repo");
 		}	
 		$tmp = tempnam(sys_get_temp_dir(),"");
 		unlink($tmp);
@@ -16,6 +17,7 @@ class gitImport {
 		$command = 	'cd '.$tmp;
 		$command.=	' && git clone "'.$repo.'"';
 		$command."\n";
+		error_log($command);
 		shell_exec($command);
 		// if (!file_exists($tmp.'/.git')) throw new Exception ('No .git Folder found');
 		$command = 'cd '.$tmp."/*/ && git log --numstat --pretty='%x1A},%x1A%H%x1A:{%x1Aauthor%x1A:%x1A%an%x1A,%x1Aauthor_mail%x1A:%x1A%ae%x1A,%x1Adate%x1A:%x1A%at%x1A,%x1Amessage%x1A:%x1A%s%x1A,%x1Achanges%x1A : %x1A'";
@@ -32,16 +34,49 @@ class gitImport {
 		self::removeDir($tmp);
 	}
 
-	private function validateURL($URL) {
-		$pattern_1 = "/^(http|https|ftp):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+.(com|de|org|net|edu|dk|at|us|tv|info|uk|co.uk|biz|se)$)(:(\d+))?\/?/i";
-      		$pattern_2 = "/^(www)((\.[A-Z0-9][A-Z0-9_-]*)+.(com|org|net|dk|at|us|tv|info|uk|co.uk|biz|se|edu|de)$)(:(\d+))?\/?/i";       
-      		if(preg_match($pattern_1, $URL) || preg_match($pattern_2, $URL)){
-		        return true;
-	      	} else {
-	        	return false;
-      		}
-    	}
+	
+  
+  /* Verify the syntax of the given URL.
+  *
+  * @access public
+  * @param $url The URL to verify.
+  * @return boolean
+  */
+private function is_valid_url($url) {
+   if ($this->str_starts_with(strtolower($url), 'http://localhost')) {
+     return true;
+   }
+   return preg_match('/^(https?):\/\/'.                                         // protocol
+'(([a-z0-9$_\.\+!\*\'\(\),;\?&=-]|%[0-9a-f]{2})+'.         // username
+'(:([a-z0-9$_\.\+!\*\'\(\),;\?&=-]|%[0-9a-f]{2})+)?'.      // password
+'@)?(?#'.                                                  // auth requires @
+')((([a-z0-9]\.|[a-z0-9][a-z0-9-]*[a-z0-9]\.)*'.           // domain segments AND
+'[a-z][a-z0-9-]*[a-z0-9]'.                                 // top level domain  OR
+'|((\d|[1-9]\d|1\d{2}|2[0-4][0-9]|25[0-5])\.){3}'.
+'(\d|[1-9]\d|1\d{2}|2[0-4][0-9]|25[0-5])'.                 // IP address
+')(:\d+)?'.                                                // port
+')(((\/+([a-z0-9$_\.\+!\*\'\(\),;:@&=-]|%[0-9a-f]{2})*)*'. // path
+'(\?([a-z0-9$_\.\+!\*\'\(\),;:@&=-]|%[0-9a-f]{2})*)'.      // query string
+'?)?)?'.                                                   // path and query string optional
+'(#([a-z0-9$_\.\+!\*\'\(\),;:@&=-]|%[0-9a-f]{2})*)?'.      // fragment
+'$/i', $url);
+}
 
+
+/**
+  * String starts with something
+  *
+  * This function will return true only if input string starts with
+  * niddle
+  *
+  * @param string $string Input string
+  * @param string $niddle Needle string
+  * @return boolean
+  */
+function str_starts_with($string, $niddle) {
+       return substr($string, 0, strlen($niddle)) == $niddle;
+}
+	
 	public function getRawRepoInfo(){
 		if (isset($this->RepoObject) && !is_null($this->RepoObject))
 			return $this->RepoObject;
@@ -51,7 +86,7 @@ class gitImport {
 	
 	private function unescape($escapedString, $escapeCharacter) {
         	// Replace bad characters with the corresponding escape sequence for JSON
-		$badCharacters = array('/"/','/\//' ,'/\\\/','/\f/','/\n/','/\r/','/\t/'); // Not supported: backslash, utf8-characters
+		$badCharacters = array('/"/','/\//' ,'/\\\/','/\f/','/\n/','/\r/','/\t/'); // Not supported: backspace, utf8-characters
 		$correspondingGoodCharacters = array('\"','\/' ,'\\','\f','\n','\r','\t');
 	        $intermediate = preg_replace($badCharacters, $correspondingGoodCharacters, $escapedString);
 	       	// Replace escapeCharacter with "
