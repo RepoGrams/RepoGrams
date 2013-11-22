7e52b1a784489f976ec57f2caa023c6925c45e8e
Marc Jose <Hering2007@web.de>
2013-11-22 13:22:39 +0100

    Changed array[xxx] in image.php

--- image.php
+++ image.php
@@ -36,7 +36,7 @@
 							if (isset($_SESSION['image'])) {
 								foreach ($_SESSION['image'] as $row) {
 									foreach ($row as $tile) {
-										echo '<li style="background-color:'.$tile['c'].';width:'.$tile['x'].';height:'.$tile['y'].'"></li>';
+										echo '<li style="background-color:'.$tile['3'].';width:'.$tile['0'].';height:'.$tile['1'].'"></li>';
 									}
 								}
 		                	}
