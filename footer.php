856f78657de800d3e9280d605f8932b55fe044ff
Heiko Becker <heiko.becker@rocketmail.com>
2013-11-29 10:57:45 +0100

    Fixed warning for unset timezone

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <div class="credit" id="footer">
 		<br>
-		<p class="muted credit">&copy;<?php echo date("Y")?> Fabian Kosmale, Heiko Becker, Maike Maas, Marc Jose, Sebastian Becking, Valerie Poser</p>		
+		<p class="muted credit">&copy;<?php date_default_timezone_set ( 'UTC' );echo date("Y")?> Fabian Kosmale, Heiko Becker, Maike Maas, Marc Jose, Sebastian Becking, Valerie Poser</p>		
 </div>
