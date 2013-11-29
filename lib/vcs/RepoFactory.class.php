9db435aa9d0cdc120b8aca9e8c89f8789babc342
Heiko Becker <heiko.becker@rocketmail.com>
2013-11-29 13:12:37 +0100

    Removed fatal error

--- lib/vcs/RepoFactory.class.php
+++ lib/vcs/RepoFactory.class.php
@@ -3,9 +3,8 @@ require_once "git/GitRepo.class.php";
 require_once __DIR__."/../../php/action.php";
 
 class RepoFactory {
-	static function createRepo($url, $callback) {
-		$act = new action();
-		$_SESSION['action']->callback();
+	static function createRepo($url) {
+		call_user_func(array('callback', 'call','aCCESSING REPO....'));
 		return new GitRepo($url);
 	}
 }
