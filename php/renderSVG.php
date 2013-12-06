9d783016720cd0c17007078b9bae6f7f575ea5c6
Maike Maas <s9memaas@stud.uni-saarland.de>
2013-12-06 12:45:52 +0100

    testing

---
+++ php/renderSVG.php
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="iso-8859-1" ?> 
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="500px" height="500px" 
+  viewBox="0 0 1000 1000"
+  xmlns="http://www.w3.org/2000/svg" version="1.1"
+  xml:lang="de"
+  color="#ff0"
+  fill="#80f"
+  stroke="#f80">
+ 
+	<title>Grundform: Rechteck</title> 
+	<desc>
+	SVG-Beispiel mit Rechtecken, 
+	abgerundete Ecken, Positionierung des 
+	Rahmens stroke und Eigenschaften der 
+	Opazität.
+	</desc>
+ 
+<rect x="5" y="5" 
+	width="990" height="990" 
+	rx="0" ry="0"
+	fill="rgb(50%,100%,100%)">
+	  <title>Rahmen und Hintergrundfarbe</title>
+	  <desc>
+	  Dieses Rechteck stellt die Hintergrundfarbe 
+	  für das Bild bereit und auch einen Rahmen 
+	  als sichtbare Begrenzung des Bildes.
+	  </desc>
+</rect>  
+  
+<rect x="100" y="100" width="300" height="200" rx="20" ry="40"
+          fill="rgb(255,0,0)" stroke="none" stroke-width="0" />
+  
+<rect x="100" y="400" width="300" height="200" rx="20" ry="40"
+          fill="#0f0" stroke="#00f" stroke-width="60" />
+  
+<rect x="500" y="200" width="300" height="200" rx="20" ry="20"
+          fill="currentColor" stroke="rgb(50,0,50)" stroke-width="20" 
+	  opacity="1" />
+
+<rect x="550" y="250" width="300" height="200" rx="20" ry="20"
+          fill="rgb(255,255,0)" stroke="rgb(0,50,50)" stroke-width="20" 
+	  fill-opacity="0.8" stroke-opacity="0.5">
+	  <desc>Beispiel für fill-opacity und stroke-opacity</desc>
+</rect>
+<rect x="600" y="300" width="300" height="200" rx="20" ry="20"
+          fill="rgb(255,255,0)" stroke="rgb(50,50,0)" stroke-width="20" 
+	  opacity="0.4">
+	  <title>Rechteck als Beispiel für opacity</title>  
+</rect>
+  
+<rect x="800" y="700" width="300" height="200" rx="40" ry="40"
+          fill="#00ffff" stroke="#ff00ff" stroke-width="50" />
+
+<rect x="100" y="700" width="300" height="200" rx="100" ry="90"
+          fill="inherit" stroke="inherit" stroke-width="20" />
+
+
+</svg>
