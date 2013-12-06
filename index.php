3326dd6dc76d45880001c4ed4f12b00df343849f
Marc Jose <Hering2007@web.de>
2013-12-06 12:42:06 +0100

    Moved helpdialog to external file

--- index.php
+++ index.php
@@ -61,29 +61,7 @@
 	</div>
 
 	<!-- Help dialog -->
-	<div class="modal fade" id="help" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  		<div class="modal-dialog">
-    		<div class="modal-content">
-      			<div class="modal-header">
-        			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        			<h4 class="modal-title">Repograms - Quick Help</h4>
-      			</div>
-      			<div class="modal-body">
-					This website renders chromograms of your git repository.<br>
-					To start just enter your repository URL and click on visualize.<br>
-					To see some examples, just click one of the icons below and then the 
-					<button class="btn btn-default" title="Visualize the provided repository" disabled>
-       					<span class="glyphicon glyphicon-indent-left"></span>Visualize!
-					</button> button. <br>
-					If you need further assistance just go to the documentation page. It can be found if
-					you click the book in the top right corner.
-      			</div>
-      			<div class="modal-footer">
-        			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      			</div>
-    		</div>
-  		</div>
-	</div>
+	<?php include('helpdialog.php')?>
 	
 	<!-- Footer -->	
 	<?php include('footer.php')?>
