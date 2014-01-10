ddcb0239f0efc989fd2bebf2cc92867d84afee7c
Maike Maas <s9memaas@stud.uni-saarland.de>
2014-01-10 13:19:32 +0100

    language: english

--- helpdialog.php
+++ helpdialog.php
@@ -1,25 +1,242 @@
+<?php session_start();
+
+  $GLOBALS["messages"] = array (
+    'en'=> array(
+      'Visualize!' 
+          => 'Visualize!',
+      'Help' 
+          => 'Help',
+      'Show me some examples' 
+          => 'Show me some examples',
+      'Enter repository url' 
+          => 'Enter repository url',
+      'Select commits from' 
+          => 'Select commits from',
+      'till' 
+          => 'till',
+      'Cloning repository into folder.' 
+          => 'Cloning repository into folder.',
+      'Abort' 
+          => 'Abort',
+      'Error! Fetching repository was not sucessfull (Invalid URL?)' 
+          => 'Error! Fetching repository was not sucessfull (Invalid URL?)',
+      'Legend' 
+          => 'Legend',
+      'Documentation' 
+          => 'Documentation',
+      'What is Repogram?' 
+          => 'What is Repogram?',
+      'The word repogram comes from the words "Repository" and "Chromograms"' 
+          => 'The word repogram comes from the words "Repository" and "Chromograms"',
+      'So what is a "Repository"?' 
+          => 'So what is a "Repository"?',
+      'A repository is a collection of source code files used to develop programs or to manage other files. If you ever used GitHub, you will surely have a so called repository.' 
+          => 'A repository is a collection of source code files used to develop programs or to manage other files. If you ever used GitHub, you will surely have a so called repository.',
+      'Ok, clear on that. What is a "Chromogram"?' 
+          => 'Ok, clear on that. What is a "Chromogram"?',
+      'A chromogram is an intelligent visualization method. It can display a huge amount of content on a small image. It usually uses blocks.' 
+          => 'A chromogram is an intelligent visualization method. It can display a huge amount of content on a small image. It usually uses blocks.',
+      'Read that. So how can I use this site?' 
+          => 'Read that. So how can I use this site?',
+      'That\'s an easy one.' 
+          => 'That\'s an easy one.',
+      'You just need to go to the frontpage, enter the URL of your repository and click on' 
+          => 'You just need to go to the frontpage, enter the URL of your repository and click on',
+      'After a short waiting time, you will see your rendered chromogram.' 
+          => 'After a short waiting time, you will see your rendered chromogram.',
+      'Do you have some examples?' 
+          => 'Do you have some examples?',
+      'Twitter Bootstrap' 
+          => 'Twitter Bootstrap',
+      'JQuery' 
+          => 'JQuery',
+      'This website renders chromograms of your repository.' 
+          => 'This website renders chromograms of your repository.',
+      'To start just enter your repository URL and click on visualize.' 
+          => 'To start just enter your repository URL and click on visualize.',
+      'To see some examples, just choose one from below and then click' 
+          => 'To see some examples, just choose one from below and then click',
+      'Visualize the provided repository' 
+          => 'Visualize the provided repository',
+      'Large repository may need some time to be downloaded and processed.' 
+          => 'Large repository may need some time to be downloaded and processed.',
+      'So please be patient and get some coffee while waiting ;)' 
+      => 'So please be patient and get some coffee while waiting ;)',
+      'Close' 
+          => 'Close',
+      'Repograms - Quick Help'
+          => 'Repograms - Quick Help'
+    ),
+ 
+    'de'=> array(
+      'Visualize!'
+      => 'Visualisieren!',
+      'Help'
+      => 'Hilfe',
+      'Show me some examples'
+      => 'Zeige mir ein paar Beispiele',
+      'Enter repository url'
+      => 'Repository URL einf&uuml;gen',
+      'Select commits from'
+      => 'Commits einfügen von',
+      'till'
+      => 'bis',
+      'Cloning repository into folder.'
+      => 'Klone Repository in Ordner.',
+      'Abort' 
+      => 'Abbrechen',
+      'Error! Fetching repository was not sucessfull (Invalid URL?)'
+      => 'Fehler! Runterladen des Repository war nicht erfolgreich (Ung&uuml;ltige URL?)',
+      'Legend'
+      => 'Legende',
+      'Documentation'
+      => 'Dokumentation',
+      'What is Repogram?'
+      => 'Was ist Repogram?',
+      'The word repogram comes from the words "Repository" and "Chromograms"'
+      => 'Das Word Repogram kommt von den W&ouml;rtern "Repository" und "Chromograms"',
+      'So what is a "Repository"?'
+      => 'Also, was ist ein "Repository"?',
+      'A repository is a collection of source code files used to develop programs or to manage other files. If you ever used GitHub, you will surely have a so called repository.'
+      => 'Ein Repository ist entweder eine Sammlung von Quell Code Dateien, welche zum Entwickeln von Programmen benutzt werden oder wird verwendet um andere Dateien zu verwalten. Hat man jemals GitHub benutzt, hat man sicher auch ein sogenanntes Repository.',
+      'Ok, clear on that. What is a "Chromogram"?'
+      => 'Ok, klar soweit. Was ist ein "Chromogram"?',
+      'A chromogram is an intelligent visualization method. It can display a huge amount of content on a small image. It usually uses blocks.'
+      => 'Ein Chromogram ist eine intelligente Visualisierungs-Methode. Es kann große Mengen an Informationen in einem Bild darstellen. Dazu werden häufig Blöcke verwendet.',
+      'Read that. So how can I use this site?'
+      => 'Auch verstanden. Wie benutze ich also diese Seite?',
+      'That\'s an easy one.'
+      => 'Das ist einfach.',
+      'You just need to go to the frontpage, enter the URL of your repository and click on'
+      => 'Auf der Hauptseite kann man eine URL einf&uumlgen und klickt: ',
+      'After a short waiting time, you will see your rendered chromogram.'
+      => 'Nach einer kurzen Bearbeitungszeit, wird das berechnete Chromogram erscheinen.',
+      'Do you have some examples?'
+      => 'Gibt es Beispiele?',
+      'Twitter Bootstrap'
+      => 'Twitter Bootstrap',
+      'JQuery'
+      => 'JQuery',
+      'This website renders chromograms of your repository.'
+      => 'Diese Webseite berechnet Chromogramme.',
+      'To start just enter your repository URL and click on visualize.'
+      => 'Zum Starten f&uuml;gt man seine Repository URL ein und klickt auf Visualisieren.',
+      'To see some examples, just choose one from below and then click'
+      => 'Um einige Beispiele zu sehen klickt man auf ',
+      'Visualize the provided repository'
+      => 'Visualisiere das vorgegebene Repository',
+      'Large repository may need some time to be downloaded and processed.'
+      => 'Große Repositories ben&ouml;tigen m&ouml;glicherweise ein wenig Zeit zum Downloaden und Bearbeiten.',
+      'So please be patient and get some coffee while waiting ;)'
+      => 'Wir bitten um ein wenig Geduld ;)',
+      'Close'
+      => 'Schlie&szlig;en',
+      'Repograms - Quick Help'
+      => 'Repograms - Schnelle Hilfe'
+    ),
+
+    'fr' => array(
+      'Visualize!' 
+          => 'Visualise!',
+      'Help' 
+          => 'Secours',
+      'Show me some examples' 
+          => 'Montrez-moi des exemples.',
+      'Enter repository url' 
+          => 'Entre repository url',
+      'Select commits from' 
+          => 's&eacute;lectionne de',
+      'till' 
+          => '&agrave;',
+      'Cloning repository into folder.' 
+          => 'Duplique repository dans un dossier.',
+      'Abort' 
+          => 'Abandon',
+      'Error! Fetching repository was not sucessfull (Invalid URL?)' 
+          => 'Erreur! Obtention r&eacute;f&eacute;rentiel n\'a pas &eacute;t&eacute; une belle r&eacute;ussite (URL invalide?)',
+      'Legend' 
+          => 'L&eacute;gende',
+      'Documentation' 
+          => 'Documentation',
+      'What is Repogram?' 
+          => 'Quelle est Repogram?',
+      'The word repogram comes from the words "Repository" and "Chromograms"' 
+          => 'Le mot repogram vient des mots "Repository" et "Chromograms"',
+      'So what is a "Repository"?' 
+          => 'Donc, ce quoi, un "Repository"?',
+      'A repository is a collection of source code files used to develop programs or to manage other files. If you ever used GitHub, you will surely have a so called repository.' 
+          => 'Un r&eacute;f&eacute;rentiel est une collection de fichiers de code source utilis&eacute; pour d&eacute;velopper des programmes ou pour g&eacute;rer d\'autres fichiers. Si vous avez d&eacute;j&agrave; utilis&eacute; GitHub, vous aurez s&ucirc;rement un r&eacute;f&eacute;rentiel dite.',
+      'Ok, clear on that. What is a "Chromogram"?' 
+          => 'Ok, clair. Qu\'est-ce qu\'un "Chromogram"?',
+      'A chromogram is an intelligent visualization method. It can display a huge amount of content on a small image. It usually uses blocks.' 
+          => 'Un Chromogram est une m&eacute;thode de visualisation intelligent. Il peut afficher une grande quantit&eacute; de contenu sur une petite image. Habituellement il utilise des blocs.',
+      'Read that. So how can I use this site?' 
+          => 'Lire. Alors, comment puis-je utiliser ce site?',
+      'That\'s an easy one.' 
+          => 'C\'est facile.',
+      'You just need to go to the frontpage, enter the URL of your repository and click on' 
+          => 'Vous avez juste besoin d\'aller &agrave; la page d\'accueil, entrez l\'URL de votre r&eacute;f&eacute;rentiel et cliquez sur',
+      'After a short waiting time, you will see your rendered chromogram.' 
+          => 'Apr&egrave;s un court temps d\'attente, vous verrez votre Chromogram rendu.',
+      'Do you have some examples?' 
+          => 'Y at-il des exemples?',
+      'Twitter Bootstrap' 
+          => 'Twitter Bootstrap',
+      'JQuery'
+          => 'JQuery',
+      'This website renders chromograms of your repository.' 
+          => 'Ce site rend chromograms de votre repository.',
+      'To start just enter your repository URL and click on visualize.' 
+          => 'Pour commencer, veuillez entrer votre URL du r&eacute;f&eacute;rentiel et cliquez sur Visualise.',
+      'To see some examples, just choose one from below and then click' 
+          => 'Pour voir quelques exemples, il suffit de choisir l\'un de ci-dessous puis cliquez sur',
+      'Visualize the provided repository'
+          => 'Visualisez le repository pr&eacute;vu',
+      'Large repository may need some time to be downloaded and processed.' 
+          => 'Grand d&eacute;p&ocirc;t peut avoir besoin de temps &agrave; t&eacute;l&eacute;charger et trait&eacute;es.',
+      'So please be patient and get some coffee while waiting ;)' 
+           => 'Alors s\'il vous plaît &ecirc;tre patient et prendre un caf&eacute; en attendant;)',
+      'Close' 
+          => 'Ferme',
+      'Repograms - Quick Help'
+          => 'Repograms - aide rapide'
+      )
+
+);
+ 
+function msg($s) {
+  $locale = 'en';
+    
+  if (isset($GLOBALS["messages"][$locale][$s])) {
+    return $GLOBALS["messages"][$locale][$s];
+  } else {
+    error_log("l10n error: locale: "."$locale, message:'$s'");
+  }
+}
+?>
+
 <div class="modal fade" id="help" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
  	<div class="modal-dialog">
     	<div class="modal-content">
       		<div class="modal-header">
        			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-       			<h4 class="modal-title">Repograms - Quick Help</h4>
+       			<h4 class="modal-title"><?php print msg('Repograms - Quick Help');?></h4>
      		</div>
       		<div class="modal-body">
-				This website renders chromograms of your git repository.<br>
-				To start just enter your repository URL and click on visualize.<br>
-				To see some examples, just choose one from below and then click
-				<button class="btn btn-default" title="Visualize the provided repository">
-       				<span class="glyphicon glyphicon-indent-left"></span>Visualize!
+				<?php print msg('This website renders chromograms of your git repository.');?><br>
+				<?php print msg('To start just enter your repository URL and click on visualize.');?><br>
+				<?php print msg('To see some examples, just choose one from below and then click');?>
+				<button class="btn btn-default" title="<?php print msg('Visualize the provided repository');?>">
+       				<span class="glyphicon glyphicon-indent-left"></span><?php print msg('Visualize!');?>
 				</button>
 				<br><br>
 				<div class="alert alert-info inforepo">
-					Large repository may need some time to be downloaded and processed.<br>
-					So please be patient and get some coffee while waiting ;)
+					<?php print msg('Large repository may need some time to be downloaded and processed.');?><br>
+					<?php print msg('So please be patient and get some coffee while waiting ;)');?>
 				</div>
       		</div>
       		<div class="modal-footer">
-        		<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        		<button type="button" class="btn btn-default" data-dismiss="modal"><?php print msg('Close');?></button>
       		</div>
     	</div>
   	</div>
