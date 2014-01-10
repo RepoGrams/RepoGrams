fc5c5f2b7bcf6dbc87df80e07dbdcc0fbf2c4794
Marc Jose <Hering2007@web.de>
2014-01-10 14:45:31 +0100

    Added flags

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <div style="float:left; margin-left: 20px; margin-top:20px;">
-	<img src="" class="flag flag-us">
-	<img src="" class="flag flag-de">
-	<img src="" class="flag flag-fr">
+	<img src="img/blank.png" class="flag flag-us">
+	<img src="img/blank.png" class="flag flag-de">
+	<img src="img/blank.png" class="flag flag-fr">
 </div>
 <div class="clear"></div>
 <div style="float:right; margin-right: 20px; margin-top:20px;">
