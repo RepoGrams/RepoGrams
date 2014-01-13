3665049d1b308a1ceb756742e9d21d2f3f09ce46
Marc Jose <Hering2007@web.de>
2014-01-13 13:58:08 +0100

    Fixed flag-positions

--- documentation.php
+++ documentation.php
@@ -13,18 +13,20 @@
 </head>
 
 <body>
-	<!-- Menu -->	
-	<div style="float:left; margin-left: 20px; margin-top:20px;">
-		<a href="php/language.php?langkey=en"><img src="img/blank.png" class="flag flag-us"></a>
-		<a href="php/language.php?langkey=de"><img src="img/blank.png" class="flag flag-de"></a>
-		<a href="php/language.php?langkey=fr"><img src="img/blank.png" class="flag flag-fr"></a>
-	</div>
-	<div class="clear"></div>
-	<div style="float:right; margin-right: 20px; margin-top:20px;">
-		<a class="btn btn-default btn-lg" href="<?php echo $_SERVER['HTTP_REFERER']; ?>" title="Go back">
-			<span class="glyphicon glyphicon-arrow-left"></span>	
-		</a>
-	</div>
+	<!-- Menu -->
+	<div>
+		<div style="float:left; margin-left: 20px; margin-top:20px;">
+			<a href="php/language.php?langkey=en"><img src="img/blank.png" class="flag flag-us"></a>
+			<a href="php/language.php?langkey=de"><img src="img/blank.png" class="flag flag-de"></a>
+			<a href="php/language.php?langkey=fr"><img src="img/blank.png" class="flag flag-fr"></a>
+		</div>
+		<div style="float:right; margin-right: 20px; margin-top:20px;">
+			<a class="btn btn-default btn-lg" href="<?php echo $_SERVER['HTTP_REFERER']; ?>" title="Go back">
+				<span class="glyphicon glyphicon-arrow-left"></span>	
+			</a>
+		</div>
+	</div>	
+
 	
 	<!-- Content -->
 	<div class="container" id="wrap">
