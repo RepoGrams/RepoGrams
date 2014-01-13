4022b6cc36a44aaf51fdb3af22fc3195a1d13148
Marc Jose <Hering2007@web.de>
2014-01-13 12:16:48 +0100

    added language cookie-variable

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <div style="float:left; margin-left: 20px; margin-top:20px;">
-	<img src="img/blank.png" class="flag flag-us">
-	<img src="img/blank.png" class="flag flag-de">
-	<img src="img/blank.png" class="flag flag-fr">
+	<img src="img/blank.png" class="flag flag-us" onclick="<?php $_COOKIE['language'] = 'en'; ?>">
+	<img src="img/blank.png" class="flag flag-de" onclick="<?php $_COOKIE['language'] = 'de'; ?>">
+	<img src="img/blank.png" class="flag flag-fr" onclick="<?php $_COOKIE['language'] = 'fr'; ?>">
 </div>
 <div class="clear"></div>
 <div style="float:right; margin-right: 20px; margin-top:20px;">
