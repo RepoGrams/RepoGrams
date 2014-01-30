14d303de3cb6c4035de65c9f18f8cb6ee5ac46a7
Maike Maas <s9memaas@stud.uni-saarland.de>
2014-01-30 13:36:12 +0100

    fix

--- php/filter.php
+++ php/filter.php
@@ -4,11 +4,11 @@ require_once("./newAlgorithm.php");
 if (isset($_GET['filter1'])) {
 	$mode = $_GET['filter1'];
 	switch ($mode) {
-		case "0": renderImage(0, 0);break;
-		case "1": renderImage(0, 1);break;
-		case "2": renderImage(0, 2);break;
-		case "3": renderImage(0, 3);break;
-		case "4": renderImage(0, 4);break;
+		case "0": reRender(0, 0);break;
+		case "1": reRender(0, 1);break;
+		case "2": reRender(0, 2);break;
+		case "3": reRender(0, 3);break;
+		case "4": reRender(0, 4);break;
 	}
 }	
 
