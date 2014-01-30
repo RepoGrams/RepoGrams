0d751bbf49873fd1883214b03cc2cb638ceec9f4
Maike Maas <s9memaas@stud.uni-saarland.de>
2014-01-30 13:41:56 +0100

    fix

--- php/filter.php
+++ php/filter.php
@@ -3,20 +3,18 @@ require_once("./newAlgorithm.php");
 
 if (isset($_GET['filter1'])) {
 	$mode = $_GET['filter1'];
+	$var = 0;
 	switch ($mode) {
-		case "0": $this->reRender(0, 0);break;
-		case "1": $this->reRender(0, 1);break;
-		case "2": $this->reRender(0, 2);break;
-		case "3": $this->reRender(0, 3);break;
-		case "4": $this->reRender(0, 4);break;
-	}
-}	
-
-function reRender($fmode = null, $smode = null) {
+		case "0": $var = 0;break;
+		case "1": $var = 1;break;
+		case "2": $var = 2;break;
+		case "3": $var = 3;break;
+		case "4": $var = 4;break;
+	}	
 	error_log($fmode.' is set to render');
 	$alg = new Algorithm();
 	$ses = unserialize($_SESSION['repo']);
-	$_SESSION['image'] = $alg->render($ses,$fmode,$smode,$_SESSION['width'], $_SESSION['height']);
+	$_SESSION['image'] = $alg->render($ses,0,$var,$_SESSION['width'], $_SESSION['height']);
 	error_log("Rendered");
 	header('Location: ../image.php');
 }
