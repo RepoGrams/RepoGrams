bbd7ba0d45776464fe69dd1231a613a7af7491bf
Maike Maas <s9memaas@stud.uni-saarland.de>
2014-01-30 12:29:26 +0100

    other conf

--- php/filter.php
+++ php/filter.php
@@ -16,7 +16,7 @@ if (isset($_GET['filter1'])) {
 }	
 
 error_log("Test");
-reRender(0,1);
+reRender(0,2);
 
 function reRender($fmode = null, $smode = null) {
 	error_log($fmode.' is set to render');
