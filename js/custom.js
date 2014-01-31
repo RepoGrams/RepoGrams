e4e3dd096b3f31ef140d91e283034636f972e6ba
Maike Maas <s9memaas@stud.uni-saarland.de>
2014-01-31 12:29:41 +0100

    language edit

--- js/custom.js
+++ js/custom.js
@@ -23,7 +23,7 @@ function example() {
 		//combine the html object
 		var head = "<div class=\"col-md-offset-4 col-md-4\">";
 		head = head + "<div class=\"panel panel-default\">";
-		head = head +"<div class=\"panel-heading\"><?php print msg ('Repository description from')?> GitHub.com:</div>";
+		head = head +"<div class=\"panel-heading\"><?php print msg ('xmpl-dis')?> GitHub.com:</div>";
 		head = head +"<div class=\"panel-body\">";
 		descr = head +descr+ "</div></div></div>";
             $("#description").html("<div style=\"text-align:center;\">"+descr+"</div>");
