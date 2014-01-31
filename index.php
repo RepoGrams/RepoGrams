55cefb49dfc7aaae3ada1a6a90d719c1a84dd5a3
Maike Maas <s9memaas@stud.uni-saarland.de>
2014-01-31 08:37:35 +0100

    back to old design

--- index.php
+++ index.php
@@ -27,70 +27,62 @@ if(!isset($_SESSION['init']) || $error){
 	
 	<!-- Content in root container-->
 	<div class="container" id="wrap"> <!-- open root container-->
-	  
-<div class="row title">
-<div id="description"></div>
-</div>
-
+		<!--<div  class="row">
+			<div class="col-xs-12 col-sm-12 col-md-offset-4 col-md-4"> --> <!--Responsive title -->
+		 		<img class="title" title="Repograms" src="img/title.png" onclick="location.href='index.php'">
+				<br>
+			<!--</div>
+		</div>-->
 	  <!--Input form  -->
-	  <div class="row">
-	  <div class="col-xs-12">
-		<form role="form" action="./loading.php" method="POST">
-			<div class="input-group urlinput <?php if ($error) echo 'has-error';?>">
-	<input class="form-control" id="repourl" name="repourl" type="text" required="required" placeholder="<?php print msg('index-enter'); ?>">
-
-      			    	
-			</div>
-
-	  		<!-- Error Handling -->
-	  		<?php 
-				if ($error) {
-					echo '<br>
-		                  <div class="alert-dismissable errormessage">
-       				  	  	<button type="button" class="close glyphicon glyphicon-remove-sign" style="float:left; right:0px;" data-dismiss="alert" aria-hidden="true"></button>
-       						<span class="help-block"><strong>&nbsp;&nbsp;'.msg('bug-error').'</strong> '.$_SESSION['error_message'].'</span>
-      				  	  </div>';
-					unset($_SESSION['error_message']);
-					initSession(true);
-				}
-			?>
-			
-			<br>
-		<div class="centerButton">
-<button class="btn btn-primary" type="submit" title="<?php print msg('index-vis'); ?>">
-       					<span class="glyphicon glyphicon-indent-left"></span><?php print msg('visualize'); ?>
-					</button>
-				<button type="button" class="btn btn-default" onclick="example();"><?php print msg('index-examples'); ?></button>
-										<button class="btn btn-info" data-toggle="modal" data-target="#help" title="<?php print msg('index-help'); ?>" type="submit">
-						<span class="glyphicon glyphicon-question-sign "></span> <?php print msg('index-help'); ?>
-					</button>
-
-		  </div>
-			<br>
-		<!-- Date picker -->
-    		<div class="datepick">
-    			<span><?php print msg('index-select'); ?> </span>
-    			<input type="text" class="input-small hasDatepicker" name="start" id="start" style="width:90px;"/>
-    			<span> <?php print msg('index-till'); ?> </span>
-    			<input type="text" class="input-small hasDatepicker" name="end" id="end"  style="width:90px;"/>
-    		</div>
-		</form>
-	  </div> <!-- collapse div for form input -->
+	  	<div class="row">
+	  		<div class="col-xs-12">
+				<form role="form" action="./loading.php" method="POST">
+					<div class="input-group urlinput <?php if ($error) echo 'has-error';?>">
+						<input class="form-control" id="repourl" name="repourl" type="text" required="required" placeholder="<?php print msg('index-enter'); ?>">
+					</div>
+			  		<!-- Error Handling -->
+			  		<?php 
+						if ($error) {
+							echo '<br>
+				                  <div class="alert-dismissable errormessage">
+		       				  	  	<button type="button" class="close glyphicon glyphicon-remove-sign" style="float:left; right:0px;" data-dismiss="alert" aria-hidden="true"></button>
+		       						<span class="help-block"><strong>&nbsp;&nbsp;'.msg('bug-error').'</strong> '.$_SESSION['error_message'].'</span>
+		      				  	  </div>';
+							unset($_SESSION['error_message']);
+							initSession(true);
+						}
+					?>
+					<br>
+					<div class="centerButton">
+						<button class="btn btn-primary" type="submit" title="<?php print msg('index-vis'); ?>">
+	       					<span class="glyphicon glyphicon-indent-left"></span><?php print msg('visualize'); ?>
+						</button>
+						<button type="button" class="btn btn-default" onclick="example();"><?php print msg('index-examples'); ?></button>
+						<button class="btn btn-info" data-toggle="modal" data-target="#help" title="<?php print msg('index-help'); ?>" type="submit">
+							<span class="glyphicon glyphicon-question-sign "></span> <?php print msg('index-help'); ?>
+						</button>
+					</div>
+					<br>
+					<!-- Date picker -->
+	    			<div class="datepick">
+		    			<span><?php print msg('index-select'); ?> </span>
+		    			<input type="text" class="input-small hasDatepicker" name="start" id="start" style="width:90px;"/>
+		    			<span> <?php print msg('index-till'); ?> </span>
+		    			<input type="text" class="input-small hasDatepicker" name="end" id="end"  style="width:90px;"/>
+	    			</div>
+				</form>
+	  		</div> <!-- collapse div for form input -->
 	  </div>
-	  <br><br>
-	  
+	  <br>
+	  <div id="description"></div>
 	  <!-- Examples -->
-	<br>
-
-	<div id="example" class="centerButton">
-	<?php require_once('./php/exampleExplained.php') ?>
+		<div id="example" class="centerButton">
+			<?php require_once('./php/exampleExplained.php') ?>
+		</div>
 	</div>
 
-
-	</div>
-
-	</div> <!-- root container close -->
-		<!-- Help dialog -->
+	<!--</div>  root container close -->
+	<!-- Help dialog -->
 	<?php include('helpdialog.php'); ?>
 	<div id="push"></div>
 
