78ff5da93ad8aaf909d22dec1936668bb6a4fc63
Maike Maas <s9memaas@stud.uni-saarland.de>
2014-01-31 11:40:42 +0100

    edit example

--- php/exampleExplained.php
+++ php/exampleExplained.php
@@ -2,22 +2,22 @@
 	<script type="text/javascript" src="js/jquery.overscroll.min.js"></script>
 		<div class="custom" style="width:400; boder-style:solid; display:inline-block;">
 			<ul style="display:inline-block; list-style-type:none !important;">
-			<li class="customBlock" id="1" style="background-color:rgb(0,128,255); width:280px; height:20px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(0,128,255); width:290px; height:20px;" data-html="true" 
 			data-original-title="This is one element of a chromogram. Hover over the different items to get detailed information."
 			data-placement="right" rel="tooltip"></li>
-			<li class="customBlock" id="1" style="background-color:rgb(260,0,66); width:100px; height:20px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(260,0,66); width:110px; height:20px;" data-html="true" 
 			data-original-title="Thats the way you do it. Each block in your chromogram represents one commit of your repository."
 			data-placement="right" rel="tooltip"></li>
 			<li class="customBlock" id="1" style="background-color:rgb(98,226,0); width:180px; height:20px;" data-html="true" 
 			data-original-title="Ok, you got the concept. The first rendering is fixed by us. The next one can be parametrized."
 			data-placement="right" rel="tooltip"></li>
-			<li class="customBlock" id="1" style="background-color:rgb(98,226,0); width:280px; height:20px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(98,226,0); width:290px; height:20px;" data-html="true" 
 			data-original-title="Ok, you got the concept. The first rendering is fixed by us. The next one can be parametrized."
 			data-placement="right" rel="tooltip"></li>
 			<li class="customBlock" id="1" style="background-color:rgb(255,226,0); width:200px; height:20px;" data-html="true" 
 			data-original-title="It's up to you to decide wether you want the author, the commit time or line changes to incluence your image."
 			data-placement="right" rel="tooltip"></li>
-			<li class="customBlock" id="1" style="background-color:rgb(0,128,255); width:80px; height:20px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(0,128,255); width:90px; height:20px;" data-html="true" 
 			data-original-title="Downloading is possible too. You can get svg, png and even pdf."
 			data-placement="right" rel="tooltip"></li>
 			<!--<li class="customBlock" id="1" style="background-color:rgb(255,0,243); width:400px; height:16px;" data-html="true" 
