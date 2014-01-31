0ea70cab91d9d31e584909b32a33455733c8132d
Heiko Becker <heiko.becker@rocketmail.com>
2014-01-31 10:47:30 +0100

    Added documentation for example function

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,28 @@
+/*
+ * function to display a random example on the index.php page
+ */
 function example() {
+	//make ajax call tp github api
 	$.ajax({
 		url: 'https://api.github.com/repositories',
-	    complete: function(xhr) {
+		//complte handler
+	    complete: function(xhr) {    
+		//take the response into a variable
 	    	var url = xhr.responseJSON;
-			var number = Math.floor(Math.random()*xhr.responseJSON.length);
-			var descr = url[number].description;
-			url = url[number].html_url;
-			var head = "<div class=\"col-md-offset-4 col-md-4\">";
-			head = head + "<div class=\"panel panel-default\">";
-			head = head +"<div class=\"panel-heading\">Repository description from GitHub.com:</div>";
-			head = head +"<div class=\"panel-body\">";
-			descr = head +descr+ "</div></div></div>";
+		//compute a random number
+		var number = Math.floor(Math.random()*xhr.responseJSON.length);
+		//get the description for the object
+		var descr = url[number].description;
+		//Get the gitHub url
+		url = url[number].html_url;
+		//combine the html object
+		var head = "<div class=\"col-md-offset-4 col-md-4\">";
+		head = head + "<div class=\"panel panel-default\">";
+		head = head +"<div class=\"panel-heading\">Repository description from GitHub.com:</div>";
+		head = head +"<div class=\"panel-body\">";
+		descr = head +descr+ "</div></div></div>";
             $("#description").html("<div style=\"text-align:center;\">"+descr+"</div>");
+	    //add the element to the document
             (document.getElementById('repourl')).value = url+'.git';
 		}
 	});
