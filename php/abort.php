9be2e300afcddc413462494c5e37c7037253674c
Heiko Becker <heiko.becker@rocketmail.com>
2014-01-31 10:40:33 +0100

    Removed unused files

--- php/abort.php
+++ php/abort.php
@@ -1,4 +1,6 @@
 <?php
+// short snippet to destroy the set session variables when the user presses the
+// abort button on the loading.php site
 $_SESSION= array();
 //session_destroy();
 header ('Location:../index.php');
