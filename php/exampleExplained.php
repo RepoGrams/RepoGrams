673788a1c14714e403ba6efda362f49276a4b175
Heiko Becker <heiko.becker@rocketmail.com>
2014-01-31 10:59:15 +0100

    Made objects smaller

--- php/exampleExplained.php
+++ php/exampleExplained.php
@@ -2,22 +2,22 @@
 	<script type="text/javascript" src="js/jquery.overscroll.min.js"></script>
 		<div class="custom" style="width:400; boder-style:solid; display:inline-block;">
 			<ul style="display:inline-block; list-style-type:none !important;">
-			<li class="customBlock" id="1" style="background-color:rgb(51,251,0); width:400px; height:24px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(51,251,0); width:400px; height:16px;" data-html="true" 
 			data-original-title="This is one element of a chromogram. Hover over the different items to get detailed information."
 			data-placement="right" rel="tooltip"></li>
-			<li class="customBlock" id="1" style="background-color:rgb(255,0,0); width:250px; height:24px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(255,0,0); width:250px; height:16px;" data-html="true" 
 			data-original-title="Thats the way you do it. Each block in your chromogram represents one commit of your repository."
 			data-placement="right" rel="tooltip"></li>
-			<li class="customBlock" id="1" style="background-color:rgb(255,137,0); width:150px; height:24px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(255,137,0); width:150px; height:16px;" data-html="true" 
 			data-original-title="Ok, you got the concept. The first rendering is fixed by us. The next one can be parametrized."
 			data-placement="right" rel="tooltip"></li>
-			<li class="customBlock" id="1" style="background-color:rgb(0,255,218); width:400px; height:24px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(0,255,218); width:400px; height:16px;" data-html="true" 
 			data-original-title="It's up to you to decide wether you want the author, the commit time or line changes to incluence your image."
 			data-placement="right" rel="tooltip"></li>
-			<li class="customBlock" id="1" style="background-color:rgb(255,0,243); width:400px; height:24px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(255,0,243); width:400px; height:16px;" data-html="true" 
 			data-original-title="Downloading is possible too. You can get svg, png and even pdf."
 			data-placement="right" rel="tooltip"></li>
-			<li class="customBlock" id="1" style="background-color:rgb(255,0,243); width:400px; height:24px;" data-html="true" 
+			<li class="customBlock" id="1" style="background-color:rgb(255,0,243); width:400px; height:16px;" data-html="true" 
 			data-original-title="If one color spans over mutiple lines and blocks, you hav a very big commit that is being linewrapped."
 			data-placement="right" rel="tooltip"></li>
 			</ul>
