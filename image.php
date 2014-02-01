7fc057a501a7e2d5f8b10da113e27d212f9f5b20
Fabian Kosmale <fakos@t-online.de>
2014-02-01 01:13:11 +0100

    reverted to uncoditionally starting the session

--- image.php
+++ image.php
@@ -6,6 +6,10 @@ if (!isset($_SESSION['image']) ) header('location: index.php');?>
 <html !DOCTYPE HTML>
 <head>
 	<?php include('header.php') ?>
+<script type="text/javascript" src="js/d3.v3.min.js"></script>
+<script src="http://code.shutterstock.com/rickshaw/vendor/d3.layout.min.js"></script>
+<script src="http://code.shutterstock.com/rickshaw/rickshaw.js"></script>
+
 </head>
 
 <body>
@@ -82,6 +86,8 @@ if (!isset($_SESSION['image']) ) header('location: index.php');?>
 				</div>
 			</div>
 
+                        <!-- GitHub visualization -->
+                        <div id="visu"></div>
 			<div id="push" class="clear"></div>
 			<br><br>
 		</div>
@@ -123,6 +129,48 @@ if (!isset($_SESSION['image']) ) header('location: index.php');?>
                   });
                   return;
                 });
+                function unixtime2date(unixtime) {
+                  // JavaScript's date is base on microseconds, unix time on 
+                  // seconds
+                  return new Date(unixtime*1000);
+                }
+                jQuery.post("php/visualization.php")
+                  .done(function(initial_data) {
+                    var data = jQuery.map(initial_data, function (arr, index) {
+                        return {x: parseInt(arr[3]), y: parseInt(arr[1])};
+                    });
+                    data.sort(function (a,b) {
+                        if (a.x < b.x) {
+                          return -1;
+                        } else if (a.x > b.x) {
+                          return 1;
+                        } else {
+                          return 0;
+                        }
+                    });
+
+                    var graph = new Rickshaw.Graph( {
+                      element: document.querySelector("#visu"),
+                        width: $("#placeOfImage").width(),
+                        height: 250,
+                        renderer: 'line',
+                        series: [ {
+                          color: 'steelblue',
+                          data: data
+                        } ]
+                    } );
+
+                    var time = new Rickshaw.Fixtures.Time();
+                    var tunit = time.unit("seconds");
+                    var axes = new Rickshaw.Graph.Axis.Time( {
+                      graph: graph,
+                      timeUnit: tunit
+                    } );
+
+                    graph.render();
+
+
+                  });
                 });               
 	</script>
 	
--- php/action.php
+++ php/action.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+error_reporting(-1);
 	require_once(__DIR__."/../lib/vcs/RepoFactory.class.php");
 	require_once(__DIR__."/algorithm.php");
 	require_once("functions.php");
