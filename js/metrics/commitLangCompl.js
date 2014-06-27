971fd7637440f5f9d3c58efffad287f9666040bd
Heiko Becker <heiko.becker@rocketmail.com>
2014-06-27 12:29:12 +0200

    Added complete implementation for #1

--- js/metrics/commitLangCompl.js
+++ js/metrics/commitLangCompl.js
@@ -23,47 +23,15 @@ if (!Array.prototype.map)
 }
 
 /*
- * Download the JSON files once into variables to have them accessible for the
- * functions:
- */
-var xmlHttp = createXmlHttpRequestObject();
-var fileEndings = null;
-var fileNames = null;
-try {
-	xmlHttp.open("GET", "/js/filenames.json");
-	xmlHttp.onreadystatechange = function() {
-		if (xmlHttp.readyState == 4 && xmlHttp.status==200){
-			fileEndings =  JSON.parse(xmlHttp.responseText)
-			alert(myJSONObject.communication[0].communication_name)
-		}
-	}
-xmlHttp.send(null);
-
-} catch (e){
-	alert("Can't connect to server:\n" + e.toString());
-}
-
-try {
-	xmlHttp.open("GET", "/js/files.json");
-	xmlHttp.onreadystatechange = function() {
-		if (xmlHttp.readyState == 4 && xmlHttp.status==200){
-			fileNames =  JSON.parse(xmlHttp.responseText)
-			alert(myJSONObject.communication[0].communication_name)
-		}
-	}
-xmlHttp.send(null);
-
-} catch (e){
-	alert("Can't connect to server:\n" + e.toString());
-}
-
-/*
  * Function that validates if a given file extension is in the set of wanted 
  * extensions, that has been specified by Ivan.
  */
-function isValidEnding(entry){
-	//TODO: check endings here from json
-	return true;
+function isValidEnding(entry, JSONvalues){
+	return jQuery.inArray(entry, JSONvalues.ENDINGS) != -1;
+}
+
+function isValidFile(entry, JSONvalues){
+	return jQuery.inArray(entry, JSONvalues.NAMES) != -1;
 }
 
 
@@ -72,22 +40,42 @@ function isValidEnding(entry){
  * changed files.
  */
 function getMetric(fileList) {
+	var mem = new Object();	
 
-	//First get a list of file extensions
-	var array = fileList.map(function(obj){
+	var result = jQuery.get("js/filenames.json", function(data){
+		var JSONvalues = jQuery.parseJSON(data);
+		
+		fileList.forEach(function (entry){
+			if(isValidFile(entry, JSONValues)){
+				entry in mem? mem[entry] +=1 : mem[entry] = 1;
+			else
+				"Other" in mem ? mem["Other"] += 1: mem[entry] = 1;
+			}
+		});
+
+		var array = fileList.map(function(obj){
 		var all = obj.split();
 		return all[all.length-1];
-		);
+			);
 
-	var mem = new Object();
-
-	array.forEach(function (entry){
-		if(isValidEnding(entry)){
-			entry in mem ? mem[entry] += 1 : mem[entry] = 1;
-		}else
-		"Other" in mem ? mem["Other"] +=1 : mem[entry] = 1;
+		array.forEach(function (entry){
+			if(isValidEnding(entry, JSONValues)){
+				entry in mem ? mem[entry] += 1 : mem[entry] = 1;
+			}else
+				"Other" in mem ? mem["Other"] +=1 : mem[entry] = 1;
+			}
+			);
+		
 	}
 	);
-
-	return mem;
+	var max = 0;
+	var Mkey = null;
+	for (var key in mem){
+		var val = mem[key];
+		if (val > max){
+			max = val;
+			Mkey = key;
+		}
+	}
+	return max;
 	}
