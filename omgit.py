a975349c0d44ed0859b3d1c49d4e5f2069a70ad6
Fabian Kosmale <fakos@t-online.de>
2014-07-01 11:08:14 +0200

    add priority queue implementation (needed for graph traversal)

--- omgit.py
+++ omgit.py
@@ -6,8 +6,23 @@ from __future__ import print_function
 import subprocess
 # import itertools
 import collections
+import heapq
 
 import networkx as nx
+
+
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+        def push(self, item, priority):
+            heapq.heappush(self._queue, (-priority, self._index, item))
+            self._index += 1
+
+        def pop(self):
+            return heapq.heappop(self._queue)[-1]
+
 
 
 def list_children(commit_id):
