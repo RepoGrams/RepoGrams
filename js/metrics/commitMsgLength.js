a6539fe35ce5e3417af325d1ce1e99b5b4593f37
Fabian Kosmale <0inkane@googlemail.com>
2014-07-02 22:51:48 +0200

    commitMsgLength.js: work per commit

--- js/metrics/commitMsgLength.js
+++ js/metrics/commitMsgLength.js
@@ -1,13 +1,4 @@
-function commitMsgLength(commitMsgs, commitSize){ // array of all commit messages, array of number of changes
-	var output = new Array(commitMsgs.length);
-	for (var i = 0; i < commitMsgs.length; i++){
-		var commit = new Array(2);
-		commit[0] = commitSize[i];
-		var commitMsg = commitMsgs[i];
-		commitMsg.trim();
-		var length = commitMsg.split(" ").length;
-		commit[1] = length;
-		output[i] = commit;
-	}
-	return output; // returns all commit messages length with number of changes in this commit
+function commitMsgLength(commitMsg, commitSize){ // array of all commit messages, array of number of changes
+	var length = commitMsg.trim().split(" ").length;
+	return [commitSize, length] ; // returns all commit messages length with number of changes in this commit
 }
