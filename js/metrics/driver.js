610c6832a653783c83229d7e6a1fbd168e964186
Fabian Kosmale <0inkane@googlemail.com>
2014-07-03 17:44:16 +0200

    more javascript fixes

--- js/metrics/driver.js
+++ js/metrics/driver.js
@@ -6,16 +6,17 @@ function runMetrics(dataAsJSON) {
     console.log(dataAsJSON);
     console.log("=============================");
   }
+  var data = dataAsJSON;
   var commit_langcomp_data = [];
   var commit_msglength_data = [];
   var commit_modularity_data = [];
   var metric5data = data.map(function(commit_datum) {
     //metric 1
-    commit_langcomp_data.push(getMetric(commit.files))
+    commit_langcomp_data.push(getMetric(commit_datum.files))
     // metric 2
-    commit_msglength_dat.push(commitMsgLength(commit_datum.commitmsg, commit_datum.churn));
+    commit_msglength_data.push(commitMsgLength(commit_datum.commitmsg, commit_datum.churn));
     // metric 3
-    commit_modularity_data.push(getMetricCommitModularity(commit_datum.files));
+    commit_modularity_data.push(getMetrictCommitModularity(commit_datum.files));
     // metric 4: TODO: not sure if implementable
     // metric 5 work on all commits
     // metric 6 is computed server side
@@ -24,5 +25,6 @@ function runMetrics(dataAsJSON) {
   });
 
   var metric5data = mostEditFile(metric5data);
+  console.log("alive");
   return [commit_langcomp_data, commit_msglength_data, commit_modularity_data, metric5data]
 }
