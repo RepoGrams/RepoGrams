934a621c3a0e81d8b1e1a892057036bf0a79d7bc
Heiko Becker <heiko.becker@rocketmail.com>
2014-08-12 21:27:24 +0200

    Code Draft

--- js/app.js
+++ js/app.js
@@ -1,28 +1,51 @@
-function RepogramsConfig($scope){
-	//default metric is 1
-	$scope.metricId = 1;
-	$scope.MetricTitle = "Metric"
-	$scope.metrics = [
-		{ metricId: 1, name : 'A', link: '#'},
-		{ metricId: 2, name : 'B', link: '#'}
-	];
-			  
-};
+var repogramsModule = angular.module('repogramsModule',[])
 
-function RepogramsRender($scope){
-	$scope.repos = [
+//
+//controllers
+//
+repogramsModule.controller('RepogramsConfig',
+	['$scope',
+	function ($scope){
+		//default metric is 1
+		$scope.metricId = 1;
+		$scope.MetricTitle = "Metric"
+		$scope.metrics = [
+			{ metricId: 1, name : 'A', link: '#'},
+			{ metricId: 2, name : 'B', link: '#'}
+		];  
+	}
+	]);
+
+repogramsModule.controller('RepogramsRender',
+	['$scope',
+	function ($scope){
+		//TODO: This is the place where the variables need to be stored!
+		$scope.repos = [
 		{ name:"jQuery",metric: "B"}
-			  ];
-};
+		];
+	}
+	]);
 
-function RepogramsImporter($scope){
+repogramsModule.controller('RepogramsImporter',
+	['$scope',function ($scope){
 	$scope.ImportButtonText = "Add";
 	$scope.importURL = "";
 	$scope.importRepo = 
-		function() {
-			if($scope.importURL == "")
-					  console.log("Use random repo now!");
-			else
-					  console.log("AJAX call to backend now!");
+	function() {
+		if($scope.importURL == "")
+			  console.log("Use random repo now!");
+		else
+			  console.log("AJAX call to backend now!");
 		};
-}
+}]);
+
+//
+//directives
+//
+repogramsModule.directive('ngRendermetric', function(){return {
+	    restrict: 'A',
+	    scope:{
+	    },
+	    template: '<div class="sparkline"><h4></h4></div>'
+		}
+});
--- visualization.php
+++ visualization.php
@@ -1,8 +1,8 @@
 <?php
-//require_once('header.php');
+require_once('header.php');
 ?>
 
-<html ng-app>
+<html ng-app="repogramsModule">
 	<head>
 		<!-- include Angular.js -->
 		<script type="text/javascript" src="/js/angular.min.js"></script>
@@ -11,6 +11,7 @@
 		<title>RepoGrams</title>
 	</head>
 	<body>
+		<!-- First block for the selection box and zoom slider -->
 		<div ng-controller="RepogramsConfig">
 			<!-- Dropdown Menu for Metric Selection -->
 			{{MetricTitle}}:
@@ -20,15 +21,15 @@
 			<!-- TODO: Add slider maybe: https://prajwalkman.github.io/angular-slider/ -->
 		</div>
 	
-		<!-- TODO: New div needed for next block?? -->
+		<!-- Main Block with the Repo name and Metric render -->
 		<div ng-controller="RepogramsRender">
 			<ul>
-				<li ng-repeat="repo in repos ">{{repo.name}} {{repo.metric}}</li>
+				<li ng-repeat="repo in repos ">{{repo.name}}</li><span ng-rendermetric></span>
 			</ul>
-			<!-- TODO: Add every single repo -->
-			<!-- TODO: Add the legend aligned right -->
+			<span ng-legend></span>
 		</div>
-		<!-- TODO: New div needed for next block?? -->
+
+		<!-- last block for import box -->
 		<div ng-controller="RepogramsImporter">
 			<input type="text" ng-model="importURL"/>
 			<button ng-click="importRepo()">{{ImportButtonText}}</button>
