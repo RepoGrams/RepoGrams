8e6775680957f3e53f88aab470f56d4725351bc1
Fabian Kosmale <fakos@t-online.de>
2014-10-06 10:47:03 +0200

    test script to run git graph without the server

---
+++ scripts/git_graph_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+import os
+import sys
+
+import git_graph as git_graph
+import githelpers as gh
+
+
+if __name__ == "__main__":
+    dirmanager = gh.DirManager()
+    cache = git_graph.GitGraphCache()
+
+    repourl = sys.argv[1]
+    print "Trying to clone {}".format(repourl)
+    try:
+        git_helper = gh.GitHelper(repourl, dirmanager)
+    except gh.GitException as e:
+        print e.message
+    g = git_graph.GitGraph(git_helper, cache)
+    print g.export()
+
