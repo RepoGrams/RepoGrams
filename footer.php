0da12ab42893d34e4b11a7597ec2b56d8b88d6f0
Heiko Becker <heikobecker92@gmail.com>
2014-10-24 17:19:09 +0200

    Remove unused php files

--- footer.php
+++
@@ -1,5 +0,0 @@
-<div id="footer" class="navbar navbar-fixed-bottom">
-	<div class="container">
-		<p class="muted credit">&copy;<?php date_default_timezone_set ( 'UTC' );echo date("Y")?> Fabian Kosmale, Heiko Becker, Maike Maas, Marc Jose, Sebastian Becking, Valerie Poser</p>	
-	</div>
-</div>
--- php/abort.php
+++
@@ -1,7 +0,0 @@
-<?php
-// short snippet to destroy the set session variables when the user presses the
-// abort button on the loading.php site
-$_SESSION= array();
-//session_destroy();
-header ('Location:../index.php');
-?>
