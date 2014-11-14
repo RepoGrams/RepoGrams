9a6417ae1016a8951d45ec78bf81bc6b1db5aa45
Heiko Becker <heikobecker92@gmail.com>
2014-11-14 22:50:32 +0100

    Add prebuild script

--- run.sh
+++ run.sh
@@ -14,3 +14,6 @@ fi
 
 echo "Running image at port $PORT."
 docker run -p $PORT:80 -p 8090:8090 repograms supervisord &
+
+echo "Running pre-caching of repositories. This may take a minute."
+./scripts/prebuild.sh
