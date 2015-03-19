3f897a150f3964c2d24ffa899ebf8bf5128730c7
Daniel Rozenberg <daniboy@gmail.com>
2015-03-18 17:41:56 -0700

    Fixed precache.sh for new docker build

--- precache.sh
+++ precache.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Running pre-caching of repositories. This may take a minute."
 
-if [ -z "$1" ]
+if [ -z "$2" ]
 then
-	echo "Usage: `basename $0` [list-of-repository-urls-file]"
+	echo "Usage: `basename $0` [domain-name] [list-of-repository-urls-file]"
 	exit 1
 fi
 
@@ -13,9 +13,9 @@ do
 	if [ -n "$REPO" ]
 	then
 		echo "| Precaching $REPO"
-		RESULT=`curl -X POST http://localhost:8090/getGitData -d "{\"repourl\":\"$REPO\"}" -H "Content-Type: application/json" --silent --show-error`
+		RESULT=`curl -X POST http://$1/getGitData -d "{\"repourl\":\"$REPO\"}" -H "Content-Type: application/json" --silent --show-error`
 		echo "+-- $RESULT" | head -c 80
 		echo " "
 	fi
-done < $1
+done < $2
 echo "Done pre-caching of repositories."
