3168ceb656cd6a2ed951c9f843626c09377218cf
Daniel Rozenberg <daniboy@gmail.com>
2015-03-19 17:24:34 -0700

    Updates to stop script to support parallel runs on same servers

--- stop.sh
+++ stop.sh
@@ -4,6 +4,7 @@ CONTAINER_IS_RUNNING=$(docker ps | grep $CONTAINER_NAME)
 if [ $? -eq 0 ]; then
 	echo "Stopping container $CONTAINER_NAME."
 	docker stop $CONTAINER_NAME
+	docker rm $CONTAINER_NAME
 else
 	echo "No container found to stop."
 fi
