c47bac6fd354f97f9d637a6c07f622f8211e5f93
frankrui <frankrui@micali.cs.ubc.ca>
2015-05-19 14:02:46 -0700

    added warning for unsupported browsers (fix for issue #95)

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -420,3 +420,35 @@ repogramsControllers.controller('RepogramsDisplayCtrl',
       reposService.moveRepositoryDown(index);
     };
   }]);
+
+// Only Chrome, Chromium, Firefox work
+repogramsControllers.controller('UnsupportedBrowserCtrl', ['$scope', function($scope) {
+    var checkBrowser = function() {
+
+	var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    	// Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+	var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+	var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    	// At least Safari 3+: "[object HTMLElementConstructor]"
+	var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+	var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+
+        if (!isFirefox && !isChrome) {
+	   return true;
+	}
+
+	return false;
+    };
+
+    $scope.data = { visible: checkBrowser() };
+
+    $scope.closeAlert = function () {
+      $scope.data.visible = false;
+    };
+
+  }]);
+
+
+
+
+    
